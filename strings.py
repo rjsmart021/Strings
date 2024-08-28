ea48cc05fdb187d9212b20c0a9f61afe200803ec
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-08-28 11:56:08 -0500

    Update strings.py

--- strings.py
+++ strings.py
@@ -12,34 +12,38 @@ reviews = [
 quotes = []
 keywords = ["good", "excellent", "bad", "poor","average"]
 for keyword in keywords:
-    print("dance")
+    words = keyword.upper()
+    print(words)
 for review in reviews:
         if keyword in keywords and keyword in review:
-                print(review)
                 quotes.append(reviews)
                 print(quotes)
 #Task2:Sentiment Tally
 positive_words = ["good", "excellent", "great", "awesome", "fantastic", "superb", "amazing"] 
 for positive_word in positive_words: 
-        print(positive_word)
-        for review in reviews:     
-                print(review.count(positive_word))
-       
+    print(positive_word)
+    print(reviews.count(positive_word))
 negative_words = ["bad", "poor", "terrible", "horrible", "awful", "disappointing", "subpar"] 
 for negative_word in negative_words: 
-        print(negative_word)
-        for review in reviews:
-                print(reviews.count(negative_word))
+    print(negative_word)
+    print(reviews.count(negative_word))
 #Task 3: Review Summary
 for review in reviews:     
         substring = review[0:31]
-        print(substring)
+        print(substring + "...")
 #2. User Input Data Processor
 #Task 1: Input Length Validator Write a script that asks for and checks the length of the 
 #user's first name and last name. Both should be at least 2 characters long.
 #If not, print an error message.
-
 User_First_name = input("Input the First Name")
-print(len(User_First_name))
+y =len(User_First_name)
+if y >= 2:
+      print(y)
+else:
+      print("First name must be more than two Characters")
 User_Last_name = input("Input the Last Name")
-print(len(User_Last_name))
+z = len(User_Last_name)
+if z >= 2:
+      print(z)
+else:
+      print("Last name must be more than two Characters")
